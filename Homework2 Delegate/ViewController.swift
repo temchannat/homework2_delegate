91167764ed2f97db780b822a82114f3a64c7cba1
Channat Tem <temchannat@gmail.com>
2017-11-21 23:41:06 +0700

    first commit

---
+++ Homework2 Delegate/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Homework2 Delegate
+//
+//  Created by Channat Tem on 11/21/17.
+//  Copyright © 2017 Channat Tem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+
+}
+

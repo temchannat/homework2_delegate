c49a0231b72de297ffb1938b5f561d39619c1acf
Channat Tem <temchannat@gmail.com>
2017-11-22 00:04:01 +0700

    finish UI

---
+++ Homework2 Delegate/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  Homework2 Delegate
+//
+//  Created by Channat Tem on 11/22/17.
+//  Copyright © 2017 Channat Tem. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
